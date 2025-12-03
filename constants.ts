395aeabf71fe4072d77e1ba263a4f6d20ee4d8ae
tywang0408-dev <tywang0408@gmail.com>
2025-12-03 13:24:20 +0800

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -5,9 +5,9 @@ import { SectionType } from './types';
 // Images
 export const IMAGES = {
   HERO_BG: "https://picsum.photos/id/1050/1920/1080",
-  OVERVIEW_GROWTH: "https://picsum.photos/id/1069/800/600",
-  OVERVIEW_BENEFITS: "https://picsum.photos/id/1070/800/600",
-  OVERVIEW_ALLOC: "https://picsum.photos/id/1071/800/600",
+  OVERVIEW_GROWTH: "https://storage.googleapis.com/zhuboquanyi/%E6%88%90%E9%95%BF%E4%BD%93%E7%B3%BB.jpeg",
+  OVERVIEW_BENEFITS: "https://storage.googleapis.com/zhuboquanyi/%E6%9D%83%E7%9B%8A%E4%BD%93%E7%B3%BB.jpeg",
+  OVERVIEW_ALLOC: "https://storage.googleapis.com/zhuboquanyi/%E6%9D%83%E7%9B%8A%E5%88%86%E9%85%8D.jpeg",
   
   // Strategy Section Images
   TRENDS_CHART: "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=1600&h=900", // Chart showing growth
